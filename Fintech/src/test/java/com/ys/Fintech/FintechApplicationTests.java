b42aa5a99105d2aa6b265575b55fc537e69dbba3
youngsik823 <youngsik823@naver.com>
2024-04-18 17:53:17 +0900

    initial commit

---
+++ Fintech/src/test/java/com/ys/Fintech/FintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.Fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
