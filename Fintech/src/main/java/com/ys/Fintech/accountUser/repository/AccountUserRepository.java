05e2f4aa9626de2cbcf41f954229ef27b9472a45
youngsik823 <youngsik823@naver.com>
2024-05-01 04:31:47 +0900

    findByName 추가

--- Fintech/src/main/java/com/ys/Fintech/accountUser/repository/AccountUserRepository.java
+++ Fintech/src/main/java/com/ys/Fintech/accountUser/repository/AccountUserRepository.java
@@ -3,6 +3,9 @@ package com.ys.Fintech.accountUser.repository;
 import com.ys.Fintech.accountUser.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountUserRepository extends JpaRepository<AccountUser, Long> {
   boolean existsByEmailOrPhoneNum(String email, String phoneNum);
+  Optional<AccountUser> findByName(String name);
 }
